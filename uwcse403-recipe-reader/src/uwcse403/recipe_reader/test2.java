32bd62058410ab1dfed13fd1ef54b1f7eaad99af
Tyler Beauchamp <tbchamp@cs.washington.edu>
2012-01-19 11:38:19 -0800

    test file main method

--- uwcse403-recipe-reader/src/uwcse403/recipe_reader/test2.java
+++ uwcse403-recipe-reader/src/uwcse403/recipe_reader/test2.java
@@ -3,5 +3,14 @@ package uwcse403.recipe_reader;
 public class test2 {
 	
 	//this is a test file to see if this is working!!
+<<<<<<< HEAD
 	//comment added by zaphans
+=======
+	
+	public static void main(String [] args) {
+		System.out.println("test");
+		System.out.println("test");
+	}
+	
+>>>>>>> test file main method
 }
