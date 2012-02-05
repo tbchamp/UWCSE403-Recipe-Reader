0727d0ca7792a742c227d0986973ebf3c97ae9e7
Zach Evans <zaphans@cs.washington.edu>
2012-02-05 00:22:10 -0800

    edited ignore file to add /bin to repo

---
+++ uwcse403-recipe-reader/bin/classes/uwcse403/recipe_reader/create_tables.sql
@@ -0,0 +1,8 @@
+create table zfr_table (
+	id integer primary key,
+	name varchar(30),
+	rating integer,
+	img_loc varchar(200)
+);
+
+insert into zfr_table values (1, "Lie Cake", 88, "http://www.russianmachineneverbreaks.com/wp-content/uploads/2011/01/jinx_portal_the-cake-is-a-lie.jpg");
