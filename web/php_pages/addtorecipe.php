023e2297b57c69763277266e7e0140bb78d7562d
unknown <Zach@Zach-PC.(none)>
2012-02-21 20:56:38 -0800

    laksflad

---
+++ web/php_pages/addtorecipe.php
@@ -0,0 +1,68 @@
+<?php
+mysql_connect("cubist.cs.washington.edu","zaphans","uvwSL97k");
+mysql_select_db("zaphans_recipe_reader");
+
+$type = $_REQUEST['type'];
+
+$unsafe_id =  $_REQUEST['recipe_id'];
+$safe_id = mysql_real_escape_string($unsafe_id);
+
+if (strcmp($type, 'ingredient') == 0){
+	$unsafe_itype =  $_REQUEST['itype'];
+	$safe_itype = mysql_real_escape_string($unsafe_itype);
+
+	$unsafe_amount =  $_REQUEST['amount'];
+	$safe_amount = mysql_real_escape_string($unsafe_amount);
+
+	$unsafe_unit =  $_REQUEST['unit'];
+	$safe_unit = mysql_real_escape_string($unsafe_unit);
+	$q = mysql_query("insert into ingredients(recipe_id, type, amount, unit)
+			values(" . $safe_id . ", '" . $safe_itype . "', " . $safe_amount
+				. ", '" . $safe_unit  . "')");
+	if (!$q) {
+		print("Add Ingredient Failed");
+	} else {
+		print("Ingredient Inserted");
+	}
+} else if (strcmp($type, 'keyword') == 0){
+	$unsafe_phrase =  $_REQUEST['phrase'];
+	$safe_phrase = mysql_real_escape_string($unsafe_phrase);
+
+	$q = mysql_query("insert into keywords values('" . $safe_phrase . "', " . $safe_id . ")");;
+	if (!$q) {
+		print("Add Keyword Failed");
+	} else {
+		print("Keyword Inserted");
+	}
+} else if (strcmp($type, 'direction') == 0){
+	$unsafe_text =  $_REQUEST['text'];
+	$safe_text = mysql_real_escape_string($unsafe_text);
+
+	$unsafe_order =  $_REQUEST['order'];
+	$safe_order = mysql_real_escape_string($unsafe_order);
+
+	$q = mysql_query("insert into instructions values(" . $safe_id . ", " . $safe_order . ", '" . $safe_text . "')");;
+	if (!$q) {
+		print("Add Direction Failed");
+	} else {
+		print("Direction Inserted");
+	}
+} else if (strcmp($type, 'note') == 0){
+	$unsafe_text =  $_REQUEST['text'];
+	$safe_text = mysql_real_escape_string($unsafe_text);
+
+	$unsafe_userid =  $_REQUEST['userid'];
+	$safe_userid = mysql_real_escape_string($unsafe_userid);
+
+	$q = mysql_query("insert into notes values(" . $safe_id . ", " . $safe_userid . ", '" . $safe_text . "')");;
+	if (!$q) {
+		print("Add Note Failed");
+	} else {
+		print("Note Inserted");
+	}
+} else {
+	print("Illegal Type");
+}
+
+mysql_close();
+?>
