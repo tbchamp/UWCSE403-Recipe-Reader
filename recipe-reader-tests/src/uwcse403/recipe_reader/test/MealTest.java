ccb8fb516a2d223ff665563a01c7cc44963e22b4
Alisa Yamanaka <yamana@cs.waashington.edu>
2012-03-09 23:03:53 -0800

    Finished writing tests for Meal. No bugs were found. w00t!

--- recipe-reader-tests/src/uwcse403/recipe_reader/test/MealTest.java
+++ recipe-reader-tests/src/uwcse403/recipe_reader/test/MealTest.java
@@ -16,9 +16,104 @@ import recipe_reader.model.Meal;
 
 public class MealTest extends AndroidTestCase {
 	
+	private Meal testMeal1;
+	private Meal testMeal2;
+	private Meal testMeal3;
+	
 	@Override
 	public void setUp() {
-
+		testMeal1 = new Meal("Breakfast");
+		testMeal2 = new Meal("Snack");
+		testMeal3 = new Meal("Dessert");
+	}
+	
+	
+	/***************************************************	CONSTRUCTOR TESTS	***************************************************/
+	
+	// Tests that the constructor creates a new Meal object
+	public void testsThatConstructorCreatesANewMealObject(){
+		assertNotNull("Tests that the constructor creates a non-null meal object.", testMeal1);
+	}
+	
+	// Tests that the constructor throws an IllegalArgumentException when passed null.
+	public void testsThatConstructorThrowsExceptionWhenPassedNullString() throws Exception {
+		try{
+			Meal testMeal = new Meal(null);
+			fail("A meal object should not be able to be made when passed null string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	// Tests that the constructor throws and IllegalArgumentException when passed an invalid string.
+	public void testsThatConstructorThrowsExceptionWhenPassedInvalidString() throws Exception {
+		try{
+			Meal testMeal = new Meal("Kitten Mittens");
+			fail("A meal object should not be able to be made when passed an invalid string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	// Tests that the constructor throws and IllegalArgumentException when passed an empty string.
+	public void testsThatConstructorThrowsExceptionWhenPassedEmptyString() throws Exception {
+		try{
+			Meal testMeal = new Meal("");
+			fail("A meal object should not be able to be made when passed an empty string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	
+	
+	/***************************************************	getIdByName TESTS	***************************************************/
+	
+	// Tests that getIdByName returns correct ID when passed valid input
+	public void testsThatGetIdByNameMethodReturnsCorrectIdForValidInput(){
+		assertEquals("Tests that getIdByName returns correct ID for a valid meal.", 2, testMeal1.getIdByName("Lunch"));
+	}
+	
+	// Tests that getIdByName throws an IllegalArgumentException when passed a null string.
+	public void testsThatGetIdByNameMethodThrowsExceptionWhenPassedANullString() throws Exception {
+		try{
+			testMeal2.getIdByName(null);
+			fail("An exception should be thrown when getIdByName is passed a null string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	// Tests that getIdByName throws an IllegalArgumentException when passed a empty string.
+	public void testsThatGetIdByNameMethodThrowsExceptionWhenPassedAnEmptyString() throws Exception {
+		try{
+			testMeal2.getIdByName("");
+			fail("An exception should be thrown when getIdByName is passed an empty string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	// Tests that getIdByName throws an IllegalArgumentException when passed a invalid string.
+	public void testsThatGetIdByNameMethodThrowsExceptionWhenPassedAnInvalidString() throws Exception {
+		try{
+			testMeal2.getIdByName("Fourth Meal");
+			fail("An exception should be thrown when getIdByName is passed an invalid string.");
+		} catch (IllegalArgumentException expected){
+		}
+	}
+	
+	
+	
+	/***************************************************	getId TESTS	***************************************************/
+	
+	// Tests that the getId method returns the correct Id
+	public void testsThatGetIdReturnsCorrectIdForAMeal(){
+		assertEquals("Tests that the getId method returns the correct ID for a Meal.", 6, testMeal3.getId());
+	}
+	
+	
+	
+	/***************************************************	getName TESTS	***************************************************/
+	
+	// Tests that the getName method returns the correct name
+	public void testsThatGetNameReturnsCorrectNameForAMeal(){
+		assertEquals("Tests that the getName method returns the correct name for a Meal.", "Snack", testMeal2.getName());
 	}
 	
 }
